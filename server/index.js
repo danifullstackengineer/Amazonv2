b9f85ed6e28a37da0b8330e26112854285ce4681
danifullstack <danifullstackengineer@gmail.com>
2022-03-10 18:00:33 +0100

    First commit

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import path from "path";
+import dotenv from "dotenv";
+import router from "./router.js";
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+if (!(process.env.NODE_ENV === "production")) {
+  app.use(cors());
+}
+
+app.use("/", router);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to database...");
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
+  })
+  .catch((err) => {
+    console.error(err);
+  });
